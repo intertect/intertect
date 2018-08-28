7d12c74c878966f6426ce4e3daa19c4118367ea7
Yash Patel <ypatel@princeton.edu>
2018-08-27 22:14:08 -0700

    Transferred routing to express

--- app.js
+++ app.js
@@ -5,13 +5,18 @@
 
 var express = require('express')
 var app = express()
+var path = require("path");
 
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+})
+
+app.get('/intro', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/intro.html'));
 })
 
 app.get('/architecture', function (req, res) {
-  res.send('hello world')
+  res.sendFile(path.join(__dirname + '/public/architecture.html'));
 })
 
 module.exports = app
