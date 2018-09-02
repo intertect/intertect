bef871646656e6a1b82194efa1336e3baad3bc70
Yash Patel <ypatel5400@gmail.com>
2018-09-01 20:49:21 -0700

    Partially moved to React

--- src/components/emptyTest.spec.js
+++ src/components/emptyTest.spec.js
@@ -1,3 +1,5 @@
+/* Need a test, otherwise the compiler complains */
+
 test('adds 1 + 2 to equal 3', () => {
-	expect(1 + 2).toBe(3); // you better believe it
+  expect(1 + 2).toBe(3); // you better believe it
 });
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 /* eslint-disable import/default */
+import React from 'react';
+import { render } from 'react-dom';
+
+import Editor from './components/Editor';
+import TerminalAssembly from './components/TerminalAssembly';
+import TerminalCat from './components/TerminalCat';
+import TerminalLs from './components/TerminalLs';
+require('./favicon.ico');
+
+render(<Editor />, document.getElementById("editor"));
+render(<TerminalAssembly />, document.getElementById("terminalAssembly"));
+render(<TerminalCat />, document.getElementById("terminalCat"));
+render(<TerminalLs />, document.getElementById("terminalLs"));
