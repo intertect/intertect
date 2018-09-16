ac5b427327c6312de0b8264f797de5ea3cc23bac
Peter DeLong <petersdelong@gmail.com>
2018-09-16 12:39:35 -0400

    Add final state of test programs
    Modify test programs slightly as well to make for better checking
    In the future, we'll check the state after every instruction execution

--- src/lesson_programs/lesson_1/part_3/prog.s
+++ src/lesson_programs/lesson_1/part_3/prog.s
@@ -1,18 +1,19 @@
-and $t7, $t2, $t3
-and $t7, $t0, $t1
+and $t4, $t2, $t3
+and $t5, $t0, $t1
 
-or $t7, $zero, $t2
+or $t6, $zero, $t2
 or $t7, $t0, $t1
 
-xor $t7, $t0, $t1
-xor $t7, $t2, $t3
+xor $t8, $t0, $t1
+xor $t9, $t2, $t3
 
-sll $t7, $t0, 1
-sll $t7, $t1, 1
-sll $t7, $t2, 1
+sll $s0, $t0, 1
+sll $s1, $t1, 1
+sll $s2, $t2, 1
 
-srl $t7, $t0, 1
-srl $t7, $t1, 1
+srl $s3, $t0, 1
+srl $s4, $t1, 1
 
-sra $t7, $t2, 1
-sra $t7, $t3, 1
+sra $s5, $t2, 1
+sra $s6, $t3, 1
+sra $s7, $a0, 1
--- src/lesson_programs/lesson_1/part_4/prog.s
+++ src/lesson_programs/lesson_1/part_4/prog.s
@@ -1,24 +1,24 @@
-addi $t7, $t0, 1
-addi $t7, $t1, 5
+addi $t5, $t0, 1
+addi $t6, $t1, 5
 addi $t7, $t4, 1
-addi $t7, $t3, 1
+addi $t8, $t3, 1
 
-addi $t7, $t0, 1
-addi $t7, $t1, 5
-addi $t7, $t4, 1
-addi $t7, $t3, 1
-addi $t7, $zero, -1
+addui $s0, $t0, 1
+addui $s1, $t1, 5
+addui $s2, $t4, 1
+addui $s3, $t3, 1
+addui $s4, $zero, -1
 
-andi $t7, $t0, 0
-andi $t7, $t0, 1
-andi $t7, $t4, 0xFFFF
+andi $s5, $t0, 0
+andi $s6, $t0, 1
+andi $s7, $t4, 0xFFFF
 
-ori $t7, $t0, 0xFFFF
-ori $t7, $t0, 1
-ori $t7, $t4, 0xFFFF
+ori $t9, $t0, 0xFFFF
+ori $a0, $t0, 1
+ori $a1, $t4, 0xFFFF
 
-xori $t7, $t0, 0xFFFF
-xori $t7, $t2, 1
-xori $t7, $t0, 1
-xori $t7, $t4, 0
-xori $t7, $t4, 1
+xori $a2, $t0, 0xFFFF
+xori $a3, $t2, 1
+xori $k0, $t0, 1
+xori $k1, $t4, 0
+xori $v0, $t4, 1
