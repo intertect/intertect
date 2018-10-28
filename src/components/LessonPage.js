59f3f7fcc9c0f14f0140e6d5b51ae5eee453b75a
Yash Patel <ypatel5400@gmail.com>
2018-10-28 08:23:11 -0700

    Separated implement section

--- src/components/LessonPage.js
+++ src/components/LessonPage.js
@@ -13,21 +13,12 @@ import YouTube from 'react-youtube';
 
 import MemoryTable from './MemoryTable.js'
 import RegistersTable from './RegistersTable.js'
+import Implement from './Implement.js'
 
 import {Memory, Registers, Latches, nameToRegisterMap} from '../utils/util.js';
 import {lessonParts, lessonContent, lessonRegisterInits, lessonAssembly,
   lessonStarterCode, lessonReferenceSolutions, lessonBinaryCode,
   lessonPipelineStudent} from '../utils/lessonItems.js';
-
-import 'brace/mode/javascript';
-import 'brace/theme/chrome';
-import 'brace/theme/dracula';
-import 'brace/theme/eclipse';
-import 'brace/theme/github';
-import 'brace/theme/monokai';
-import 'brace/theme/solarized_dark';
-import 'brace/theme/solarized_light';
-import 'brace/theme/twilight';
 
 import '../styles/intro.css';
 
@@ -106,11 +97,7 @@ class LessonPage extends Component {
       showMemory: (this.props.lesson != 1 || this.props.lessonPart > 5),
       showRegisters: true,
       unviewedStepExplanation: true,
-      unviewedImplementExplanation: true,
       unviewedMemoryExplanation: true,
-      showTest: false,
-
-      theme: "solarized_dark",
 
       programRunning: false
     }
@@ -490,23 +477,6 @@ class LessonPage extends Component {
       stepExplanation = <div></div>;
     }
 
-    var implementExplanation;
-    if (this.state.unviewedImplementExplanation) {
-      implementExplanation =
-        <Popover placement="right" component="a" popoverBody={pulsatingInterest}>
-          <PopoverHeader></PopoverHeader>
-          <PopoverBody>This is where you{"'"}ll be implementing your code for the lesson! We{"'"}ve provided
-          you with a template to get started.
-            <Button outline style={{width:"100%"}}
-              onClick={() => this.setState({ unviewedImplementExplanation : false })}>
-              <i className="fa fa-stop" aria-hidden="true"></i> Close Help
-            </Button>
-          </PopoverBody>
-        </Popover>
-    } else {
-      implementExplanation = <div></div>
-    }
-
     var memoryExplanation;
     if (this.state.unviewedMemoryExplanation) {
       memoryExplanation =
@@ -543,7 +513,6 @@ class LessonPage extends Component {
                   <Button outline onClick={() => {
                       this.setState({
                         isIntroPaneOpen: true,
-                        showTest: false,
                         revealCompletedLevels: false
                       });
 
@@ -637,7 +606,6 @@ class LessonPage extends Component {
                     onClick={() => {
                     this.setState({
                       isIntroPaneOpen: true,
-                      showTest: false
                     });
 
                     this.saveProgram(this.state.lesson,
@@ -707,66 +675,10 @@ class LessonPage extends Component {
         </Modal>
 
         <div className="row" >
-          <div className="col-sm-6" style={{display: "flex"}}>
-            <Card style={{ marginTop: '1rem', width:"100%"}} className="text-center">
-              <CardHeader color="default-color">
-                {implementExplanation}
-                <CardTitle componentclassName="h4">
-                  Implement
-                </CardTitle>
-              </CardHeader>
-              <CardBody>
-                <AceEditor
-                  mode="javascript"
-                  theme={this.state.theme}
-                  onChange={this.onChange}
-                  name="UNIQUE_ID"
-                  editorProps={{$blockScrolling: true}}
-                  value={this.state.studentProgram}
-                  width="100%"
-                  tabSize="2"
-                  style={{
-                    visible : !this.state.showTest,
-                    zIndex: 0,
-                    display: "flex"
-                  }}
-                />
-                <br />
-                <Dropdown>
-                  <DropdownToggle caret outline color="default" style={{width:"100%"}}>
-                    {this.state.theme.replace(/_/g," ")}
-                  </DropdownToggle>
-                  <DropdownMenu  style={{width:"100%"}}>
-                    <DropdownItem onClick={() => {this.setState({ theme : "chrome" })}} eventKey="chrome" className={this.state.theme == "chrome" ? "active" : "inactive"}>chrome</DropdownItem>
-                    <DropdownItem onClick={() => {this.setState({ theme : "dracula" })}} eventKey="dracula" className={this.state.theme == "dracula" ? "active" : "inactive"}>dracula</DropdownItem>
-                    <DropdownItem onClick={() => {this.setState({ theme : "eclipse" })}} eventKey="eclipse" className={this.state.theme == "eclipse" ? "active" : "inactive"}>eclipse</DropdownItem>
-                    <DropdownItem onClick={() => {this.setState({ theme : "github" })}} eventKey="github" className={this.state.theme == "github" ? "active" : "inactive"}>github</DropdownItem>
-                    <DropdownItem onClick={() => {this.setState({ theme : "monokai" })}} eventKey="monokai" className={this.state.theme == "monokai" ? "active" : "inactive"}>monokai</DropdownItem>
-                    <DropdownItem onClick={() => {this.setState({ theme : "solarized_dark" })}} eventKey="solarized_dark" className={this.state.theme == "solarized_dark" ? "active" : "inactive"}>solarized (dark)</DropdownItem>
-                    <DropdownItem onClick={() => {this.setState({ theme : "solarized_light" })}} eventKey="solarized_light" className={this.state.theme == "solarized_light" ? "active" : "inactive"}>solarized (light)</DropdownItem>
-                    <DropdownItem onClick={() => {this.setState({ theme : "twilight" })}} eventKey="twilight" className={this.state.theme == "twilight" ? "active" : "inactive"}>twilight</DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-                <br />
-                <div className="row">
-                  <div className="col-sm-6">
-                    <Button outline style={{width:"100%"}}
-                        onClick={() => this.saveProgram(this.state.lesson,
-                          this.state.lessonPart, this.state.starterProgram)}>
-                      <i className="fa fa-save" aria-hidden="true"></i> Save Code
-                    </Button>
-                  </div>
-
-                  <div className="col-sm-6">
-                    <Button outline color="danger" style={{width:"100%"}}
-                        onClick={() => {this.setState({ confirmRestart : true })}}>
-                      <i className="fa fa-warning" aria-hidden="true"></i> Restart Level
-                    </Button>
-                  </div>
-                </div>
-              </CardBody>
-            </Card>
-          </div>
+          <Implement
+            theme={"solarized_dark"}
+            onChange={this.onChange}
+            studentProgram={this.state.studentProgram} />
 
           <div className="col-sm-6">
             <Card style={{ marginTop: '1rem', width:"100%"}}>
